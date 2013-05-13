c9346f866617b2e7297742fed8436aeb96e43633
Kevin Candelaria <kevdashdev@gmail.com>
2013-05-13 23:33:42 +0800

    Generated the rails app, pending first Integration Test

---
+++ rails_basics/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBasics::Application.config.secret_key_base = 'a84fbbfd3e94b212465ab034e6b1494c4bbd2a82c9bea6618b75344059a3ec2cc2fc33400df0bfd125762c04101eb03d8c6ad9f612e90abb20a34c0460d3969e'
---
+++ rails_basics/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsBasics::Application.config.session_store :cookie_store, key: '_rails_basics_session'
