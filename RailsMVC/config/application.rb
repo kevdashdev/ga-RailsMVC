d9eed3cdcceb3d329db7b2c530d5a75b161d13cf
Kevin Candelaria <kevdashdev@gmail.com>
2013-05-15 01:15:59 +0800

    Finish Database Configuration section

--- RailsMVC/config/application.rb
+++ RailsMVC/config/application.rb
@@ -2,7 +2,7 @@
 require File.expand_path '../boot', __FILE__
 
 # the action controller that serves the requests
-require 'action_controller/railtie'
+require 'rails/all'
 
 # Libraries required for the application to run
 # include :default and current environment libraries
@@ -13,8 +13,8 @@ module RailsMVC
   class Application < Rails::Application
     # Configure the default encoding used in templates for Ruby 1.9
     config.encoding = 'utf-8'
+    # enable the asset pipeline
+    config.assets.enabled = true
   end
 end
 
-# enable the asset pipeline
-config.assets.enabled = true
