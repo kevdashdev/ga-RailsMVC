d00a55f25a32ee00be70f701ff9f682c791ab732
Kevin Candelaria <kevdashdev@gmail.com>
2013-05-15 00:56:15 +0800

    Copy skeleton built in from previous app, Rails-Basics

---
+++ RailsMVC/config/application.rb
@@ -0,0 +1,17 @@
+# Load libraries required on boot
+require File.expand_path '../boot', __FILE__
+
+# the action controller that serves the requests
+require 'action_controller/railtie'
+
+# Libraries required for the application to run
+# include :default and current environment libraries
+Bundler.require :default, Rails.env
+
+# app itself
+module HelloWorld
+  class Application < Rails::Application
+    # Configure the default encoding used in templates for Ruby 1.9
+    config.encoding = 'utf-8'
+  end
+end
